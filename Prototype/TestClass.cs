7cc25e96e88442ccb6a4a17be37aa5668be9b555
Starinin Andrey <starininandrey@gmail.com>
2023-05-16 18:58:32 +0300

    Initial commit

---
+++ Prototype/TestClass.cs
@@ -0,0 +1,30 @@
+﻿namespace Prototype;
+
+public class TestClass : ICloneable
+{
+    public int Id { get; set; }
+
+    public TestClass(int id)
+    {
+        Id = id;
+    }
+
+    public TestClass()
+    {
+    }
+
+    public TestClass(TestClass test)
+    {
+        Id = test.Id;
+    }
+
+    public TestClass Clone()
+    {
+        return new TestClass(Id);
+    }
+
+    object ICloneable.Clone()
+    {
+        return Clone();
+    }
+}
